3f3e22508302bf5c339fc0847a603784b2aab1f9
marco76tv <marco.sottana@gmail.com>
2021-11-30 10:43:28 +0100

    .

---
+++ Http/Livewire/Editor.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Themes\LaravelIo\Http\Livewire;
+
+use Livewire\Component;
+
+class Editor extends Component {
+    public $label;
+
+    public $placeholder = 'Write a reply...';
+
+    public $body;
+
+    public $hasButton;
+
+    public $buttonType = 'submit';
+
+    public $buttonLabel = 'Submit';
+
+    public $buttonIcon;
+
+    public function render() {
+        $this->body = old('body', $this->body);
+
+        return view('livewire.editor');
+    }
+
+    public function getPreviewProperty() {
+        return replace_links(md_to_html($this->body ?: ''));
+    }
+
+    public function preview() {
+        $this->emit('previewRequested');
+    }
+}
