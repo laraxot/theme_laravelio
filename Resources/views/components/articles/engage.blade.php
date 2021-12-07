e5a5a4e8f8c25de7aadaad4c2824d2f7de9477d9
marco76tv <marco.sottana@gmail.com>
2021-12-07 12:31:05 +0100

    .

--- Resources/views/components/articles/engage.blade.php
+++ Resources/views/components/articles/engage.blade.php
@@ -4,9 +4,9 @@
 
         <div class="flex flex-col items-center gap-y-5 mt-10">
             <span class="uppercase text-gray-500">Share</span>
-
+            {{--  
             <a class="text-gray-300 hover:text-twitter" target="_blank" rel="noopener" aria-label="Share on Twitter"
-                href="http://twitter.com/share?text={{ urlencode('"' . $article->title() . '" by ' . ($article->author()->twitter() ? '@' . $article->author()->twitter() : $article->author()->name()) . ' - ') }}&url={{ urlencode(route('articles.show', $article->slug())) }}">
+                href="http://twitter.com/share?text={{ urlencode('"' . $article->title() . '" by ' . ($article->author->twitter() ? '@' . $article->author()->twitter() : $article->author()->name()) . ' - ') }}&url={{ urlencode(route('articles.show', $article->slug())) }}">
                 <x-theme::svg icon="twitter" class="w-6 h-6" />
             </a>
 
@@ -19,6 +19,7 @@
                 href="https://www.linkedin.com/shareArticle?mini=true&url={{ urlencode(route('articles.show', $article->slug())) }}&title={{ urlencode('"' . $article->title() . '" by ' . $article->author()->name() . ' - ') }}">
                 <x-svg icon="linkedin" class="w-6 h-6" />
             </a>
+            --}}
         </div>
     </div>
 </div>
