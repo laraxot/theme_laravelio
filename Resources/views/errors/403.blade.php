2fc64325bf51203c6a3775d0af8c8df10f9cd248
marco76tv <marco.sottana@gmail.com>
2021-12-13 19:21:59 +0100

    .

--- Resources/views/errors/403.blade.php
+++ Resources/views/errors/403.blade.php
@@ -4,6 +4,10 @@
 @section('body')
     <div class="my-20 text-center text-gray-800">
         <h1 class="text-5xl">{{-- $title??$message --}}</h1>
-        <p class="text-lg">You're not allowed to this page.</p>
+        <p class="text-lg">You're not allowed to this page.
+            {{ $message ?? '' }}
+            {{ $msg ?? '' }}
+
+        </p>
     </div>
 @endsection
