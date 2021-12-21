e64ea18cfe57152d1c378aed084eae0ccd33b017
Malebestia <malebestia@yahoo.it>
2021-12-21 20:55:37 +0100

    .

--- Resources/views/pages/thread-create.blade.php
+++ Resources/views/pages/thread-create.blade.php
@@ -1,4 +1,10 @@
 @extends('pub_theme::layouts.base')
 @section('body')
-    @livewire('forum::thread.create', ['model_name' => 'thread'])
+    <main class="max-w-4xl mx-auto pt-10 pb-12 px-4 lg:pb-16">
+        <div class="lg:grid lg:gap-x-5">
+            <div class="sm:px-6 lg:px-0 lg:col-span-9">
+                @livewire('forum::thread.create', ['model_name' => 'thread'])
+            </div>
+        </div>
+    </main>
 @endsection
