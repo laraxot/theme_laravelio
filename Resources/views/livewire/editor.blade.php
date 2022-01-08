0a5da18c1f08f14a4b5db46b2a5912971d61b4bf
Malebestia <malebestia@yahoo.it>
2022-01-08 20:45:47 +0100

    .

--- Resources/views/livewire/editor.blade.php
+++ Resources/views/livewire/editor.blade.php
@@ -43,7 +43,12 @@
 
                             @if ($buttonIcon)
                                 <span class="ml-1">
-                                    @svg($buttonIcon, 'w-5 h-5')
+                                    {{-- @svg($buttonIcon,'w-5h-5') --}}
+                                    <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                            clip-rule="evenodd" />
+                                    </svg>
                                 </span>
                             @endif
                         </span>
