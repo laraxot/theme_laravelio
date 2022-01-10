8f1d3f9a3c5d26a742153cdc7b1523fde15fe3ca
Malebestia <malebestia@yahoo.it>
2022-01-10 21:58:49 +0100

    .

--- Resources/views/pages/show.blade.php
+++ Resources/views/pages/show.blade.php
@@ -1,10 +1,3 @@
-@php
-//proposta per spostare il controllo nelle policy del modello interessato e non in pagepanelpolicy:
-//se ti piace, perchè non mettere questa blade in theme::pages.show, invece di scrivere un page.show in ogni tema?
-
-//$model = xotModel(Str::remove('-create', $params['item0']));
-//$_panel = Panel::get($model);
-@endphp
 @php($act = Str::camel($row->guid))
 @can($act, $_panel)
     @include('pub_theme::pages.'.$row->guid)
