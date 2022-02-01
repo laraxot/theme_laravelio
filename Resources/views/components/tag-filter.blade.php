3615c30b560c67af1b1e8396e1922ce41e51a0f6
marco76tv <marco.sottana@gmail.com>
2022-02-01 16:11:32 +0100

    .

--- Resources/views/components/tag-filter.blade.php
+++ Resources/views/components/tag-filter.blade.php
@@ -1,7 +1,7 @@
 @props(['activeTag', 'tags', 'filter'])
 
 <div class="flex flex-col bg-white rounded-md shadow max-h-full"
-    x-data="{ activeTag: '{{ $activeTag ? $activeTag->id() : null }}', filter: '', isFiltered(value) { return !this.filter || value.toLowerCase().includes(this.filter.toLowerCase()) } }"
+    x-data="{ activeTag: '{{ $activeTag ? $activeTag->id : null }}', filter: '', isFiltered(value) { return !this.filter || value.toLowerCase().includes(this.filter.toLowerCase()) } }"
     x-cloak>
     <div class="border-b">
         <div class="p-4">
@@ -31,13 +31,12 @@
     <div class="border-b overflow-y-scroll">
         <div class="flex flex-col text-lg p-4">
             @foreach ($tags as $tag)
-                <a href="{{-- route('forum.tag',[$tag->slug(),'filter'=>$filter]) --}}"
-                    class="flex items-center py-3.5 hover:text-lio-500"
-                    :class="{ 'text-lio-500': '{{ $tag->id() }}' === activeTag }"
+                <a href="{{-- route('forum.tag',[$tag->slug(),'filter'=>$filter]) --}}" class="flex items-center py-3.5 hover:text-lio-500"
+                    :class="{ 'text-lio-500': '{{ $tag->id }}' === activeTag }"
                     x-show="isFiltered('{{ $tag->name() }}')">
                     {{ $tag->name() }}
                     <x-svg icon="o-check" -circle class="ml-3 w-6 h-6 text-lio-500" x-cloak
-                        x-show="'{{ $tag->id() }}' === activeTag" />
+                        x-show="'{{ $tag->id }}' === activeTag" />
                 </a>
             @endforeach
         </div>
@@ -46,10 +45,10 @@
     <div class="flex gap-x-2 justify-end p-4">
         <x-buttons.secondary-button @click="$dispatch('close-modal')">
             Cancel
-            </x-buttons.secondary-button>
+        </x-buttons.secondary-button>
 
-            <x-buttons.secondary-button href="{{-- route('forum') --}}" x-show="activeTag">
-                Remove filter
-                </x-buttons.secondary-button>
+        <x-buttons.secondary-button href="{{-- route('forum') --}}" x-show="activeTag">
+            Remove filter
+        </x-buttons.secondary-button>
     </div>
 </div>
