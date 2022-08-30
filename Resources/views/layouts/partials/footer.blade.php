61c752ecfa6933973d9054a1abc699944a3b6314
marco76tv <marco.sottana@gmail.com>
2022-08-30 14:00:45 +0200

    up

--- Resources/views/layouts/partials/footer.blade.php
+++ Resources/views/layouts/partials/footer.blade.php
@@ -9,17 +9,17 @@
 					<span>@lang('pub_theme::footer.order_delivery_takeout')</span> <br/><br/><br/>
 					<h5>@lang('pub_theme::footer.follow_me')</h5>
                             <span itemscope itemtype="http://schema.org/Organization">
-                            <link itemprop="url" href="https://www.foodfriendfinder.com">
-                                 <a href="{{ url('http://www.facebook.com/foodfriendfindercom') }}" class="btn btn-social-icon btn-facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+                            <link itemprop="url" href="https://www.test sitename.com">
+                                 <a href="{{ url('http://www.facebook.com/test sitenamecom') }}" class="btn btn-social-icon btn-facebook" target="_blank"><i class="fa fa-facebook"></i></a>
                                 <!-- <a href="{{ url('http://www.twitter.com/FoodFriendFinde') }}" class="btn btn-social-icon btn-twitter" target="_blank"><i class="fa fa-twitter"></i></a>
                                  <a href="{{ url('https://plus.google.com/104035968244380319088') }}" class="btn btn-social-icon btn-google-plus" target="_blank"><i class="fa fa-google-plus"></i></a>-->
-                                 <a href="{{ url('https://www.instagram.com/foodfriendfinder') }}" class="btn btn-social-icon btn-instagram" target="_blank"><i class="fa fa-instagram"></i></a>
-                                 <!--  <a href="{{ url('https://www.linkedin.com/company/foodfriendfinder/') }}" class="btn btn-social-icon btn-linkedin" target="_blank"><i class="fa fa-linkedin"></i></a><br/>
-                                 <a href="{{ url('https://foodfriendfinder.tumblr.com/') }}" class="btn btn-social-icon btn-tumblr" target="_blank"><i class="fa fa-tumblr"></i></a>
+                                 <a href="{{ url('https://www.instagram.com/test sitename') }}" class="btn btn-social-icon btn-instagram" target="_blank"><i class="fa fa-instagram"></i></a>
+                                 <!--  <a href="{{ url('https://www.linkedin.com/company/test sitename/') }}" class="btn btn-social-icon btn-linkedin" target="_blank"><i class="fa fa-linkedin"></i></a><br/>
+                                 <a href="{{ url('https://test sitename.tumblr.com/') }}" class="btn btn-social-icon btn-tumblr" target="_blank"><i class="fa fa-tumblr"></i></a>
                                  <a href="{{ url('https://www.youtube.com/channel/UCqi2naxxvPIRBn-fGov1-ww') }}" class="btn btn-social-icon btn-youtube" target="_blank"><i class="fa fa-youtube"></i></a>
-                                 <a href="{{ url('https://myspace.com/foodfriendfinder') }}" class="btn btn-social-icon btn-user" target="_blank"><i class="fa fa-users"></i></a>
-                                 <a href="{{ url('https://it.pinterest.com/foodfriendfinder/') }}" class="btn btn-social-icon btn-pinterest-p" target="_blank"><i class="fa fa-pinterest-p"></i></a>
-                                 <a href="{{ url('https://soundcloud.com/foodfriendfinder') }}" class="btn btn-social-icon btn-soundcloud" target="_blank"><i class="fa fa-soundcloud"></i></a>-->
+                                 <a href="{{ url('https://myspace.com/test sitename') }}" class="btn btn-social-icon btn-user" target="_blank"><i class="fa fa-users"></i></a>
+                                 <a href="{{ url('https://it.pinterest.com/test sitename/') }}" class="btn btn-social-icon btn-pinterest-p" target="_blank"><i class="fa fa-pinterest-p"></i></a>
+                                 <a href="{{ url('https://soundcloud.com/test sitename') }}" class="btn btn-social-icon btn-soundcloud" target="_blank"><i class="fa fa-soundcloud"></i></a>-->
                                     </span>
 				</div>
 			{{--
