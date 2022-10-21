7489a0c3098663bc55712aafae02c613b537be2f
marco76tv <marco.sottana@gmail.com>
2022-10-21 07:14:45 +0000

    up

--- Resources/views/layouts/_nav.blade.php
+++ Resources/views/layouts/_nav.blade.php
@@ -4,7 +4,7 @@
             <div class="lg:px-4 lg:flex">
                 <div class="block lg:flex lg:items-center lg:flex-shrink-0">
                     <div class="flex justify-between items-center p-4 lg:p-0">
-                        <a href="{{ route('home') }}" class="mr-4">
+                        <a href="{{ url('/') }}" class="mr-4">
                             <img class="h-6 w-auto lg:h-8"
                                 src="{{ Theme::asset('pub_theme::images/laravelio-logo.svg') }}" alt="" />
                         </a>
